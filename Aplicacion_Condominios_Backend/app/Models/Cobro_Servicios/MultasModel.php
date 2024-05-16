bd1ccea4a68d8c3e4e81214f7104c0bdb741a7d6
Josue <josue7pedro@gmail.com>
2024-05-16 19:19:06 -0400

    cambios del main

--- Aplicacion_Condominios_Backend/app/Models/Cobro_Servicios/MultasModel.php
+++ Aplicacion_Condominios_Backend/app/Models/Cobro_Servicios/MultasModel.php
@@ -4,17 +4,17 @@ namespace App\Models\Cobro_Servicios;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Cobro_Servicios\ExpensaModel;
+use App\Models\Cobro_Servicios\PreAvisoModel;
 
 class MultasModel extends Model
 {
     use HasFactory;
     protected $table = 'multas';
 
-    protected $fillable = ['expensa_id', 'descripcion', 'monto', 'fecha'];
+    protected $fillable = ['preaviso_id', 'descripcion', 'monto', 'fecha'];
 
-    public function expensa()
+    public function preaviso()
     {
-        return $this->belongsTo(ExpensaModel::class, 'expensa_id');
+        return $this->belongsTo(PreAvisoModel::class, 'preaviso_id');
     }
 }
